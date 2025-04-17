c3b1116b10f8af3f35a418a5775449f905858673
Phil Boivin <phil@philboivin.com>
2025-04-17 09:50:13 -0400

    - added collectstatic to the entrypoint script - update to base.html to load JavaScript only for registered user - front end fixes

--- poker/entrypoint.sh
+++ poker/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
 # Run npm install (for Tailwind) in the static_src directory at container startup
 cd /code/poker/theme/static_src && npm install
+
+# Collect static files
+cd /code/poker && python manage.py collectstatic --noinput
+
 exec "$@"
